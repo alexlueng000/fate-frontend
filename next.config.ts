15c896843ea83cb4bf0ddb9e3f51214fb96f06b5
alexlueng000 <alexlueng000@gmail.com>
2025-08-25 00:34:18 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // 把你实际访问的来源都写上（含协议与端口）
+    allowedDevOrigins: [
+      'http://119.29.135.234',
+      'http://119.29.135.234:80',
+      'http://119.29.135.234:3000',
+      // 如果用 https，也加上：
+      // 'https://119.29.135.234'
+    ],
+  },
 };
 
 export default nextConfig;
